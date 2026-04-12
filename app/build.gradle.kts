5c15e640cd573b74bac6e43aaa81529f82902c88
Brandon <brandon@example.com>
2026-04-12 10:12:11 -0400

    Revert versionName to 0.0.4-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 4
-        versionName = "0.1.0"
+        versionName = "0.0.4-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
