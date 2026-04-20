625062856c0d81fd7c5acb270abb3a716d1af71e
th3cavalry <th3.cavalry@gmail.com>
2026-04-19 21:59:32 -0400

    fix: resolve detekt configuration errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.th3cavalry.androidllm"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "0.4.0"
+        versionCode = 16
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
